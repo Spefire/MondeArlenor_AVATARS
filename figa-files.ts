a217acf118d223cdb3c98da24fd2df176b8b9c7d
Nicholas BRUN <nicholasb@hotmail.fr>
2024-03-01 17:09:51 +0100

    Rajout des types de couleurs pour FIGA

--- figa-files.ts
+++ figa-files.ts
@@ -1,24 +1,31 @@
+export class ETypeColors {
+  public static Hair = 'HAIR';
+  public static Skin = 'SKIN';
+  public static Other = 'OTHER';
+}
+
 export const characterItems: AssetsItem[] = [
-  { libelle: 'Corps', name: 'body', nb: 1 },
-  { libelle: 'Haut', name: 'tops/top', nb: 3 },
-  { libelle: 'Bas', name: 'bottoms/bottom', nb: 3 },
-  { libelle: 'Chaussures', name: 'shoes', nb: 1 },
-  { libelle: 'Bouche', name: 'mouth', nb: 1 },
-  { libelle: 'Yeux', name: 'eyes', nb: 1 },
-  { libelle: 'Sourcils', name: 'eyebrow', nb: 1 },
-  { libelle: 'Cils', name: 'eyelashes', nb: 1 },
-  { libelle: 'Cheveux', name: 'hairs/hair', nb: 5 },
+  { libelle: 'Corps', name: 'body', nb: 1, typeColors: ETypeColors.Skin },
+  { libelle: 'Haut', name: 'tops/top', nb: 3, typeColors: ETypeColors.Other },
+  { libelle: 'Bas', name: 'bottoms/bottom', nb: 3, typeColors: ETypeColors.Other },
+  { libelle: 'Chaussures', name: 'shoes', nb: 1, typeColors: ETypeColors.Other },
+  { libelle: 'Bouche', name: 'mouth', nb: 1, typeColors: null },
+  { libelle: 'Yeux', name: 'eyes', nb: 1, typeColors: null },
+  { libelle: 'Sourcils', name: 'eyebrow', nb: 1, typeColors: null },
+  { libelle: 'Cils', name: 'eyelashes', nb: 1, typeColors: null },
+  { libelle: 'Cheveux', name: 'hairs/hair', nb: 5, typeColors: ETypeColors.Hair },
 ];
 
 export const floorsItems: AssetsItem[] = [
-  { libelle: 'Sol', name: 'grounds/ground', nb: 8 },
-  { libelle: 'Fleurs', name: 'flowers/flowers', nb: 3 },
-  { libelle: 'Détails', name: 'details/details', nb: 12 },
-  { libelle: 'Lumières féériques', name: 'faery_lights/faery_lights', nb: 2 },
+  { libelle: 'Sol', name: 'grounds/ground', nb: 8, typeColors: ETypeColors.Other },
+  { libelle: 'Fleurs', name: 'flowers/flowers', nb: 3, typeColors: ETypeColors.Other },
+  { libelle: 'Détails', name: 'details/details', nb: 12, typeColors: ETypeColors.Other },
+  { libelle: 'Lumières féériques', name: 'faery_lights/faery_lights', nb: 2, typeColors: ETypeColors.Other },
 ];
 
 export class AssetsItem {
   public libelle: string;
   public name: string;
   public nb: number;
+  public typeColors: string;
 }
