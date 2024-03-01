386e1855bd01d77e49a6d470c63d854c98c8ee0c
Nicholas BRUN <nicholasb@hotmail.fr>
2024-03-01 12:35:01 +0100

    Reorganisation et ajout des fichiers pour figa

--- figa-files.ts
+++ figa-files.ts
@@ -1,26 +1,24 @@
-export class ETypeColors {
-  public static Hair = 'HAIR';
-  public static Skin = 'SKIN';
-  public static Other = 'OTHER';
-}
-
 export const characterItems: AssetsItem[] = [
-  { category: null, libelle: 'Corps', name: 'body', nb: 1, removable: false, typeColors: ETypeColors.Skin },
-  { category: null, libelle: 'Haut', name: 'tops/top', nb: 3, removable: true, typeColors: ETypeColors.Other },
-  { category: null, libelle: 'Bas', name: 'bottoms/bottom', nb: 3, removable: true, typeColors: ETypeColors.Other },
-  { category: null, libelle: 'Chaussures', name: 'shoes', nb: 1, removable: true, typeColors: ETypeColors.Other },
-  { category: null, libelle: 'Bouche', name: 'mouth', nb: 1, removable: false, typeColors: null },
-  { category: null, libelle: 'Yeux', name: 'eyes', nb: 1, removable: false, typeColors: null },
-  { category: null, libelle: 'Sourcils', name: 'eyebrow', nb: 1, removable: false, typeColors: null },
-  { category: null, libelle: 'Cils', name: 'eyelashes', nb: 1, removable: false, typeColors: null },
-  { category: null, libelle: 'Cheveux', name: 'hairs/hair', nb: 5, removable: true, typeColors: ETypeColors.Hair },
+  { libelle: 'Corps', name: 'body', nb: 1 },
+  { libelle: 'Haut', name: 'tops/top', nb: 3 },
+  { libelle: 'Bas', name: 'bottoms/bottom', nb: 3 },
+  { libelle: 'Chaussures', name: 'shoes', nb: 1 },
+  { libelle: 'Bouche', name: 'mouth', nb: 1 },
+  { libelle: 'Yeux', name: 'eyes', nb: 1 },
+  { libelle: 'Sourcils', name: 'eyebrow', nb: 1 },
+  { libelle: 'Cils', name: 'eyelashes', nb: 1 },
+  { libelle: 'Cheveux', name: 'hairs/hair', nb: 5 },
+];
+
+export const floorsItems: AssetsItem[] = [
+  { libelle: 'Sol', name: 'grounds/ground', nb: 8 },
+  { libelle: 'Fleurs', name: 'flowers/flowers', nb: 3 },
+  { libelle: 'Détails', name: 'details/details', nb: 12 },
+  { libelle: 'Lumières féériques', name: 'faery_lights/faery_lights', nb: 2 },
 ];
 
 export class AssetsItem {
-  public category: string;
   public libelle: string;
   public name: string;
   public nb: number;
-  public removable: boolean;
-  public typeColors: string;
 }
