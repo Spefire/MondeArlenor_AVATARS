39685ec9e3158c2cf5ec8e53a0dd7b052653f99e
Nicholas BRUN <nicholasb@hotmail.fr>
2024-03-29 20:20:22 +0100

    Renommages, traduction et ajout des icones

--- figa-files.ts
+++ figa-files.ts
@@ -5,26 +5,26 @@ export class ETypeColors {
 }
 
 export const characterItems: AssetsItem[] = [
-  { libelle: 'Corps', name: 'bodys/body', nb: 2, typeColors: null },
-  { libelle: 'Body Add-ons', name: 'body_add-ons/addons', nb: 3, typeColors: null },
-  { libelle: 'Haut', name: 'tops/top', nb: 3, typeColors: ETypeColors.Other },
-  { libelle: 'Bas', name: 'bottoms/bottom', nb: 3, typeColors: ETypeColors.Other },
-  { libelle: 'Chaussures', name: 'shoes', nb: 1, typeColors: ETypeColors.Other },
-  { libelle: 'Bouche', name: 'mouth', nb: 1, typeColors: null },
-  { libelle: 'Yeux', name: 'eyes', nb: 1, typeColors: null },
-  { libelle: 'Sourcils', name: 'eyebrow', nb: 1, typeColors: null },
-  { libelle: 'Cils', name: 'eyelashes', nb: 1, typeColors: null },
-  { libelle: 'Cornes', name: 'horns', nb: 7, typeColors: null },
-  { libelle: 'Queues', name: 'tails', nb: 3, typeColors: null },
-  { libelle: 'Ailes', name: 'wings', nb: 2, typeColors: null },
-  { libelle: 'Cheveux', name: 'hairs/hair', nb: 5, typeColors: ETypeColors.Hair },
+  { libelle: 'Bodies', name: 'bodies/body', nb: 2, typeColors: ETypeColors.Skin },
+  { libelle: 'Body (add-ons)', name: 'body_addons/addons', nb: 3, typeColors: ETypeColors.Other },
+  { libelle: 'Tops', name: 'tops/top', nb: 3, typeColors: ETypeColors.Other },
+  { libelle: 'Bottoms', name: 'bottoms/bottom', nb: 3, typeColors: ETypeColors.Other },
+  { libelle: 'Shoes', name: 'shoes', nb: 1, typeColors: ETypeColors.Other },
+  { libelle: 'Mouth', name: 'mouth', nb: 1, typeColors: null },
+  { libelle: 'Eyes', name: 'eyes', nb: 1, typeColors: null },
+  { libelle: 'Eyebrows', name: 'eyebrows', nb: 1, typeColors: null },
+  { libelle: 'Eyelashes', name: 'eyelashes', nb: 1, typeColors: null },
+  { libelle: 'Horns', name: 'horns/horns', nb: 7, typeColors: ETypeColors.Other },
+  { libelle: 'Tails', name: 'tails/tails', nb: 3, typeColors: ETypeColors.Other },
+  { libelle: 'Wings', name: 'wings/wings', nb: 2, typeColors: ETypeColors.Other },
+  { libelle: 'Hairs', name: 'hairs/hair', nb: 5, typeColors: ETypeColors.Hair },
 ];
 
 export const floorsItems: AssetsItem[] = [
-  { libelle: 'Sol', name: 'grounds/ground', nb: 8, typeColors: ETypeColors.Other },
-  { libelle: 'Fleurs', name: 'flowers/flowers', nb: 3, typeColors: ETypeColors.Other },
-  { libelle: 'Détails', name: 'details/details', nb: 12, typeColors: ETypeColors.Other },
-  { libelle: 'Lumières féériques', name: 'faery_lights/faery_lights', nb: 2, typeColors: ETypeColors.Other },
+  { libelle: 'Grounds', name: 'grounds/ground', nb: 8, typeColors: ETypeColors.Other },
+  { libelle: 'Flowers', name: 'flowers/flowers', nb: 3, typeColors: ETypeColors.Other },
+  { libelle: 'Details', name: 'details/details', nb: 12, typeColors: ETypeColors.Other },
+  { libelle: 'Faery lights', name: 'faery_lights/faery_lights', nb: 2, typeColors: ETypeColors.Other },
 ];
 
 export class AssetsItem {
